1421356574bfe192e362408b3767ae8b5ba153e3
FiguringToCode <marineryash2000@gmail.com>
2024-11-04 19:28:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function secondsToMinutesSeconds(seconds) {
 
 
 const getSongs = async () => {
-    let files = await fetch("songs/")
+    let files = await fetch("https://figuringtocode.github.io/TuneWave/songs/")
     let response = await files.text()
     let div = document.createElement("div")
     div.innerHTML = response
